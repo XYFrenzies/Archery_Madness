bde9683570896113e3cff6f942408aaf2056e268
Ben <bennybill16@gmail.com>
2021-05-31 10:00:31 +1000

    I have added a shooting and camera movement script. In order to use these scripts, attach the camera script to any object in scene and give the camera component to the script whilst also adding the sphere prefab in the prefabs folder to the game and set it inactive. Then the controls will be mouse and WASD to move the camera in the space and press left click to shoot.

---
+++ Main_Game/Assets/Scripts/Test_Scene_Scripts/Camera_Movement/Camera_Movement.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_Movement : MonoBehaviour
+{
+    private Vector3 moveVector;
+    [SerializeField]private float moveSpeed = 100.0f;
+    private float rawAxisHorz = 0;
+    private float rawAxisVert = 0;
+    [SerializeField] private GameObject camObj;
+
+    [SerializeField] private float speedHorizontalMouse = 2.0f;
+    [SerializeField] private float speedVerticalMouse = 2.0f;
+
+    private float yaw = 0.0f;
+    private float pitch = 0.0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        moveVector = new Vector3(0,0,0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rawAxisHorz = Input.GetAxisRaw("Horizontal");
+        rawAxisVert = Input.GetAxisRaw("Vertical");
+        yaw += speedHorizontalMouse * Input.GetAxis("Mouse X");
+        pitch -= speedVerticalMouse * Input.GetAxis("Mouse Y");
+        moveVector.x = rawAxisHorz;
+        moveVector.y = rawAxisVert;
+        Vector3 input = camObj.transform.right * moveVector.x + camObj.transform.forward * moveVector.y;
+        if (rawAxisHorz != 0 || rawAxisVert != 0)
+            camObj.transform.position += moveSpeed * input * Time.deltaTime;
+        camObj.transform.eulerAngles = new Vector3(pitch, yaw, 0.0f);
+    }
+    
+}
---
+++ Main_Game/Assets/Scripts/Test_Scene_Scripts/Shooting/OnShot.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnShot : MonoBehaviour
+{
+    [SerializeField] private GameObject shotPosition = null;
+    [SerializeField] private GameObject objToShoot = null;
+    // Update is called once per frame
+    void Update()
+    {
+        if (objToShoot != null && Input.GetMouseButton(0))
+        {
+            objToShoot.transform.position = shotPosition.transform.position;
+            objToShoot.transform.rotation = shotPosition.transform.rotation;
+            objToShoot.SetActive(true);
+        }
+    }
+}
